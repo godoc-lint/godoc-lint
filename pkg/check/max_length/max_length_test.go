510605ef9ccbefc5c4c4e047763f7be6ad1a4e6e
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2025-02-28 19:15:25 +0000

    refactor: remove ineffective tests
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>
    
    The tests are redundant because all checkers must implement the
    interface, otherwise the compiler will complain.

--- pkg/check/max_length/max_length_test.go
+++
@@ -1,8 +0,0 @@
-package max_length_test
-
-import (
-	"github.com/godoc-lint/godoc-lint/pkg/check/max_length"
-	"github.com/godoc-lint/godoc-lint/pkg/model"
-)
-
-var _ model.Checker = &max_length.MaxLengthChecker{}
--- pkg/check/package_doc/package_doc_test.go
+++
@@ -1,8 +0,0 @@
-package package_doc_test
-
-import (
-	"github.com/godoc-lint/godoc-lint/pkg/check/package_doc"
-	"github.com/godoc-lint/godoc-lint/pkg/model"
-)
-
-var _ model.Checker = &package_doc.PackageDocChecker{}
