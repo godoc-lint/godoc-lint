6c6279f2036c0d03465ce7d9b8da1418582942d9
Babak K. Shandiz <babakks@github.com>
2025-08-03 13:15:40 +0100

    test: add test for cmd package exclusion from `pkg-doc` rule
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

---
+++ testdata/rule/pkg_doc/command_pkg/good_test.go
@@ -0,0 +1,11 @@
+// some header
+
+// Foo is a fake command.
+//
+// This test is to ensure that the godoc for command packages (i.e. `main`
+// packages) are not enforced to begin with "Package main".
+//
+// See for more details:
+//   - https://github.com/godoc-lint/godoc-lint/issues/10
+//   - https://go.dev/doc/comment#cmd
+package main
