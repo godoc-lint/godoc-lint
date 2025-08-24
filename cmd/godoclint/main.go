9197c5ea780d7d9fdaafb538f5237798ee97f892
Babak K. Shandiz <babakks@github.com>
2025-08-24 22:18:38 +0100

    refactor: move flag definition to main
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- cmd/godoclint/main.go
+++ cmd/godoclint/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/tools/go/analysis/singlechecker"
 
@@ -10,6 +12,7 @@ import (
 	"github.com/godoc-lint/godoc-lint/pkg/check"
 	"github.com/godoc-lint/godoc-lint/pkg/config"
 	"github.com/godoc-lint/godoc-lint/pkg/inspect"
+	"github.com/godoc-lint/godoc-lint/pkg/model"
 	"github.com/godoc-lint/godoc-lint/pkg/version"
 )
 
@@ -31,10 +34,61 @@ func main() {
 	inspector := inspect.NewInspector(ocb, exitFunc)
 	analyzer := analysis.NewAnalyzer(baseDir, ocb, reg, inspector, exitFunc)
 
+	configOverride := model.NewConfigOverride()
+	ocb.SetOverride(configOverride)
+
+	analyzer.GetAnalyzer().Flags.Func("config", "path to config file", func(s string) error {
+		if configOverride.ConfigFilePath != nil {
+			return errors.New("config file is set multiple times")
+		}
+		if strings.TrimSpace(s) == "" {
+			return errors.New("empty path")
+		}
+		configOverride.ConfigFilePath = &s
+		return nil
+	})
+
+	walkNonEmptyCSV := func(f func(string)) func(string) error {
+		return func(value string) error {
+			values := strings.Split(strings.TrimSpace(value), ",")
+			for _, v := range values {
+				if strings.TrimSpace(v) == "" {
+					return errors.New("empty element")
+				}
+				f(v)
+			}
+			return nil
+		}
+	}
+
+	walkNonEmpty := func(f func(string)) func(string) error {
+		return func(value string) error {
+			if strings.TrimSpace(value) == "" {
+				return errors.New("empty value")
+			}
+			f(value)
+			return nil
+		}
+	}
+
+	analyzer.GetAnalyzer().Flags.Func("include", "regexp path (Unix style) to include (can be used multiple times)", walkNonEmpty(func(s string) {
+		configOverride.Include = append(configOverride.Include, s)
+	}))
+	analyzer.GetAnalyzer().Flags.Func("exclude", "regexp path (Unix style) to exclude (can be used multiple times)", walkNonEmpty(func(s string) {
+		configOverride.Exclude = append(configOverride.Exclude, s)
+	}))
+	analyzer.GetAnalyzer().Flags.Func("enable", "comma-separated rule names to enable", walkNonEmptyCSV(func(s string) {
+		configOverride.Enable = append(configOverride.Enable, s)
+	}))
+	analyzer.GetAnalyzer().Flags.Func("disable", "comma-separated rule names to disable", walkNonEmptyCSV(func(s string) {
+		configOverride.Disable = append(configOverride.Disable, s)
+	}))
+
 	analyzer.GetAnalyzer().Flags.BoolFunc("V", "print version and exit", func(s string) error {
 		fmt.Println(version.Current)
 		os.Exit(0)
 		return nil
 	})
+
 	singlechecker.Main(analyzer.GetAnalyzer())
 }
--- pkg/analysis/analyzer.go
+++ pkg/analysis/analyzer.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"path/filepath"
-	"strings"
 
 	"golang.org/x/tools/go/analysis"
 
@@ -44,56 +43,6 @@ func NewAnalyzer(baseDir string, cb model.ConfigBuilder, reg model.Registry, ins
 			Requires: []*analysis.Analyzer{inspector.GetAnalyzer()},
 		},
 	}
-
-	configOverride := model.NewConfigOverride()
-	cb.SetOverride(configOverride)
-
-	result.analyzer.Flags.Func("config", "path to config file", func(s string) error {
-		if configOverride.ConfigFilePath != nil {
-			return errors.New("config file is set multiple times")
-		}
-		if strings.TrimSpace(s) == "" {
-			return errors.New("empty path")
-		}
-		configOverride.ConfigFilePath = &s
-		return nil
-	})
-
-	walkNonEmptyCSV := func(f func(string)) func(string) error {
-		return func(value string) error {
-			values := strings.Split(strings.TrimSpace(value), ",")
-			for _, v := range values {
-				if strings.TrimSpace(v) == "" {
-					return errors.New("empty element")
-				}
-				f(v)
-			}
-			return nil
-		}
-	}
-
-	walkNonEmpty := func(f func(string)) func(string) error {
-		return func(value string) error {
-			if strings.TrimSpace(value) == "" {
-				return errors.New("empty value")
-			}
-			f(value)
-			return nil
-		}
-	}
-
-	result.analyzer.Flags.Func("include", "regexp path (Unix style) to include (can be used multiple times)", walkNonEmpty(func(s string) {
-		configOverride.Include = append(configOverride.Include, s)
-	}))
-	result.analyzer.Flags.Func("exclude", "regexp path (Unix style) to exclude (can be used multiple times)", walkNonEmpty(func(s string) {
-		configOverride.Exclude = append(configOverride.Exclude, s)
-	}))
-	result.analyzer.Flags.Func("enable", "comma-separated rule names to enable", walkNonEmptyCSV(func(s string) {
-		configOverride.Enable = append(configOverride.Enable, s)
-	}))
-	result.analyzer.Flags.Func("disable", "comma-separated rule names to disable", walkNonEmptyCSV(func(s string) {
-		configOverride.Disable = append(configOverride.Disable, s)
-	}))
 
 	result.analyzer.Run = result.run
 	return result
