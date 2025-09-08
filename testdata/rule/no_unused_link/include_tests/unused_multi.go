b0b9b66b024d6138549251ffacd32b1eed356ba8
Babak K. Shandiz <babakks@github.com>
2025-09-08 20:11:26 +0100

    fix: improve `no-unused-link` rule issue message
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- testdata/rule/no_unused_link/include_tests/unused_multi.go
+++ testdata/rule/no_unused_link/include_tests/unused_multi.go
@@ -1,36 +1,36 @@
 // some header
 
-// godoc with multiple unused links // want `godoc has unused link \(link1\)` `godoc has unused link \(link2\)`
+// godoc with multiple unused links // want `godoc has unused link \("link1"\)` `godoc has unused link \("link2"\)`
 //
 // [link1]: https://foo.com
 // [link2]: https://foo.com
 package no_unused_link
 
-// godoc with multiple unused links // want `godoc has unused link \(link1\)` `godoc has unused link \(link2\)`
+// godoc with multiple unused links // want `godoc has unused link \("link1"\)` `godoc has unused link \("link2"\)`
 //
 // [link1]: https://foo.com
 // [link2]: https://foo.com
 const MultipleUnusedLinkConst = 0
 
-// godoc with multiple unused links // want `godoc has unused link \(link1\)` `godoc has unused link \(link2\)`
+// godoc with multiple unused links // want `godoc has unused link \("link1"\)` `godoc has unused link \("link2"\)`
 //
 // [link1]: https://foo.com
 // [link2]: https://foo.com
 const (
-	// godoc with multiple unused links // want `godoc has unused link \(link1\)` `godoc has unused link \(link2\)`
+	// godoc with multiple unused links // want `godoc has unused link \("link1"\)` `godoc has unused link \("link2"\)`
 	//
 	// [link1]: https://foo.com
 	// [link2]: https://foo.com
 	MultiMultipleUnusedLinkConst = 0
 )
 
-// godoc with multiple unused links // want `godoc has unused link \(link1\)` `godoc has unused link \(link2\)`
+// godoc with multiple unused links // want `godoc has unused link \("link1"\)` `godoc has unused link \("link2"\)`
 //
 // [link1]: https://foo.com
 // [link2]: https://foo.com
 type TMultipleUnusedLink int
 
-// godoc with multiple unused links // want `godoc has unused link \(link1\)` `godoc has unused link \(link2\)`
+// godoc with multiple unused links // want `godoc has unused link \("link1"\)` `godoc has unused link \("link2"\)`
 //
 // [link1]: https://foo.com
 // [link2]: https://foo.com
@@ -38,7 +38,7 @@ type (
 	TMultiMultipleUnusedLink int
 )
 
-// godoc with multiple unused links // want `godoc has unused link \(link1\)` `godoc has unused link \(link2\)`
+// godoc with multiple unused links // want `godoc has unused link \("link1"\)` `godoc has unused link \("link2"\)`
 //
 // [link1]: https://foo.com
 // [link2]: https://foo.com
--- testdata/rule/no_unused_link/normal/unused.go
+++ testdata/rule/no_unused_link/normal/unused.go
@@ -1,38 +1,38 @@
 // some header
 
-// godoc with unused link // want `godoc has unused link \(link\)`
+// godoc with unused link // want `godoc has unused link \("link"\)`
 //
 // [link]: https://foo.com
 package no_unused_link
 
-// godoc with unused link // want `godoc has unused link \(link\)`
+// godoc with unused link // want `godoc has unused link \("link"\)`
 //
 // [link]: https://foo.com
 const UnusedLinkConst = 0
 
-// godoc with unused link // want `godoc has unused link \(link\)`
+// godoc with unused link // want `godoc has unused link \("link"\)`
 //
 // [link]: https://foo.com
 const (
-	// godoc with unused link // want `godoc has unused link \(link\)`
+	// godoc with unused link // want `godoc has unused link \("link"\)`
 	//
 	// [link]: https://foo.com
 	MultiUnusedLinkConst = 0
 )
 
-// godoc with unused link // want `godoc has unused link \(link\)`
+// godoc with unused link // want `godoc has unused link \("link"\)`
 //
 // [link]: https://foo.com
 type TUnusedLink int
 
-// godoc with unused link // want `godoc has unused link \(link\)`
+// godoc with unused link // want `godoc has unused link \("link"\)`
 //
 // [link]: https://foo.com
 type (
 	TMultiUnusedLink int
 )
 
-// godoc with unused link // want `godoc has unused link \(link\)`
+// godoc with unused link // want `godoc has unused link \("link"\)`
 //
 // [link]: https://foo.com
 func UnusedLinkFunc() {}
