d13471d98727c470bce453d9dbd58de5d1d8c8cd
Babak K. Shandiz <babakks@github.com>
2025-09-08 20:43:26 +0100

    chore: bump version
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,9 +5,9 @@ import "fmt"
 // Current represents the current version.
 var Current = Version{
 	Major:  0,
-	Minor:  7,
+	Minor:  8,
 	Patch:  0,
-	Suffix: "",
+	Suffix: "dev",
 }
 
 // Version represents module version (in semver format).
