badb51c7a920fd1db873cb08c6a93800cf7fe930
Babak K. Shandiz <babakks@github.com>
2025-09-08 20:03:08 +0100

    fix: improve `start-with-name` rule issue message
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- testdata/rule/start_with_name/include_tests/missing_pkg_test.go
+++ testdata/rule/start_with_name/include_tests/missing_pkg_test.go
@@ -2,26 +2,26 @@ package include_tests_test
 
 // (BG: bad godoc)
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("TestFooBG"\)`
 const TestFooBG = 0
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("TestOwlBG"\)`
 var TestOwlBG = 0
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("TestCatBG"\)`
 type TestCatBG int
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("TESTYoloBG"\)`
 func TESTYoloBG() {}
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("testFooBG"\)`
 const testFooBG = 0
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("testOwlBG"\)`
 var testOwlBG = 0
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("testCatBG"\)`
 type testCatBG int
 
-// godoc // want `godoc should start with symbol name \(pattern "%"\)`
+// godoc // want `godoc should start with symbol name \("testYoloBG"\)`
 func testYoloBG() {}
