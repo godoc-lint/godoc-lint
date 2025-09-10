dd7157e646de7094b7fd11b1d42856d10d10d398
Babak K. Shandiz <babakks@github.com>
2025-09-10 23:25:00 +0100

    test: add test for `PlainConfig.Validate`
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- pkg/config/plain_test.go
+++ pkg/config/plain_test.go
@@ -1,6 +1,7 @@
 package config_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -82,4 +83,53 @@ func TestTransferOptions(t *testing.T) {
 			require.Equal(tt.expected, target)
 		})
 	}
+}
+
+func TestValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		pcfg    *config.PlainConfig
+		wantErr []string
+	}{
+		{
+			name: "zero",
+			pcfg: &config.PlainConfig{},
+		},
+		{
+			name: "default",
+			pcfg: config.GetDefaultPlainConfig(),
+		},
+		{
+			name: "invalid",
+			pcfg: &config.PlainConfig{
+				Default: ptr("foo"),
+				Enable:  []string{"foo", "bar", "baz"},
+				Disable: []string{"foo", "bar", "baz"},
+				Include: []string{"(", ")"},
+				Exclude: []string{"(", ")"},
+			},
+			wantErr: []string{
+				`invalid default set "foo"; must be one of ["all" "basic" "none"]`,
+				`invalid rule name(s) to enable: ["foo" "bar" "baz"]`,
+				`invalid rule name(s) to disable: ["foo" "bar" "baz"]`,
+				`invalid inclusion pattern(s): ["(" ")"]`,
+				`invalid exclusion pattern(s): ["(" ")"]`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.pcfg.Validate()
+			if tt.wantErr == nil {
+				require.NoError(t, err)
+			} else {
+				require.ErrorContains(t, err, strings.Join(tt.wantErr, "\n"))
+			}
+		})
+	}
+}
+
+func ptr[T any](v T) *T {
+	return &v
 }
