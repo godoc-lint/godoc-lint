80cf5453dd0802b5d0da520f0fb65ef9f5ac04c2
Babak K. Shandiz <babakks@github.com>
2025-09-10 23:06:11 +0100

    test: add default config test
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

---
+++ testdata/rule/default_config/fail/fail.go
@@ -0,0 +1,7 @@
+// some header
+
+// bad godoc // want `package godoc should start with "Package default_config "` `package has more than one godoc \("default_config"\)`
+package default_config
+
+// bad godoc // want `godoc should start with symbol name \("Foo"\)`
+const Foo = 0
