589e5a83abe7596c14b8cd0149ea1a667699d7c2
Babak K. Shandiz <babakks@github.com>
2025-10-14 21:06:45 +0100

    chore: release v0.10.1
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,9 +6,9 @@ import "fmt"
 // Current represents the current version.
 var Current = Version{
 	Major:  0,
-	Minor:  11,
-	Patch:  0,
-	Suffix: "dev",
+	Minor:  10,
+	Patch:  1,
+	Suffix: "",
 }
 
 // Version represents module version (in semver format).
