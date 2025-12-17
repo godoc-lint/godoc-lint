52f7e6955a86b03ab9ccd3b323fe61f7a5e59ce1
Babak K. Shandiz <babakks@github.com>
2025-12-17 21:28:27 +0000

    feat: add `max-len/ignore-patterns` option
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- pkg/config/builder_test.go
+++ pkg/config/builder_test.go
@@ -15,6 +15,86 @@ import (
 
 var _ model.ConfigBuilder = &config.ConfigBuilder{}
 
+func TestTransferPrimitiveOptions(t *testing.T) {
+	uintPtr := func(x uint) *uint {
+		return &x
+	}
+
+	def, err := config.FromYAML(config.DefaultConfigYAML)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name     string
+		sources  []*config.PlainRuleOptions
+		expected *model.RuleOptions
+	}{
+		{
+			name:     "empty",
+			sources:  []*config.PlainRuleOptions{{}},
+			expected: &model.RuleOptions{},
+		},
+		{
+			name: "empty and then non-empty",
+			sources: []*config.PlainRuleOptions{{}, {
+				MaxLenLength: uintPtr(999),
+			}},
+			expected: &model.RuleOptions{
+				MaxLenLength: 999,
+			},
+		},
+		{
+			name: "non-empty and then empty",
+			sources: []*config.PlainRuleOptions{{
+				MaxLenLength: uintPtr(999),
+			}, {}},
+			expected: &model.RuleOptions{
+				MaxLenLength: 999,
+			},
+		},
+		{
+			name: "non-empty and then non-empty",
+			sources: []*config.PlainRuleOptions{{
+				MaxLenLength: uintPtr(888),
+			}, {
+				MaxLenLength: uintPtr(999),
+			}},
+			expected: &model.RuleOptions{
+				MaxLenLength: 999,
+			},
+		},
+		{
+			name:    "default",
+			sources: []*config.PlainRuleOptions{def.Options},
+			expected: &model.RuleOptions{
+				MaxLenLength:                   77,
+				MaxLenIncludeTests:             false,
+				PkgDocIncludeTests:             false,
+				SinglePkgDocIncludeTests:       false,
+				RequirePkgDocIncludeTests:      false,
+				RequireDocIncludeTests:         false,
+				RequireDocIgnoreExported:       false,
+				RequireDocIgnoreUnexported:     true,
+				StartWithNameIncludeTests:      false,
+				StartWithNameIncludeUnexported: false,
+				NoUnusedLinkIncludeTests:       false,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			target := &model.RuleOptions{}
+			for _, source := range tt.sources {
+				config.TransferPrimitiveOptions(target, source)
+			}
+
+			require.Equal(tt.expected, target)
+		})
+	}
+}
+
 func TestConfigResolution(t *testing.T) {
 	tests := []struct {
 		name                     string
--- pkg/config/plain_test.go
+++ pkg/config/plain_test.go
@@ -7,83 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/godoc-lint/godoc-lint/pkg/config"
-	"github.com/godoc-lint/godoc-lint/pkg/model"
 )
-
-func TestTransferOptions(t *testing.T) {
-	uintPtr := func(x uint) *uint {
-		return &x
-	}
-
-	def, err := config.FromYAML(config.DefaultConfigYAML)
-	require.NoError(t, err)
-
-	tests := []struct {
-		name     string
-		sources  []*config.PlainRuleOptions
-		expected *model.RuleOptions
-	}{{
-		name:     "empty",
-		sources:  []*config.PlainRuleOptions{{}},
-		expected: &model.RuleOptions{},
-	}, {
-		name: "empty and then non-empty",
-		sources: []*config.PlainRuleOptions{{}, {
-			MaxLenLength: uintPtr(999),
-		}},
-		expected: &model.RuleOptions{
-			MaxLenLength: 999,
-		},
-	}, {
-		name: "non-empty and then empty",
-		sources: []*config.PlainRuleOptions{{
-			MaxLenLength: uintPtr(999),
-		}, {}},
-		expected: &model.RuleOptions{
-			MaxLenLength: 999,
-		},
-	}, {
-		name: "non-empty and then non-empty",
-		sources: []*config.PlainRuleOptions{{
-			MaxLenLength: uintPtr(888),
-		}, {
-			MaxLenLength: uintPtr(999),
-		}},
-		expected: &model.RuleOptions{
-			MaxLenLength: 999,
-		},
-	}, {
-		name:    "default",
-		sources: []*config.PlainRuleOptions{def.Options},
-		expected: &model.RuleOptions{
-			MaxLenLength:                   77,
-			MaxLenIncludeTests:             false,
-			PkgDocIncludeTests:             false,
-			SinglePkgDocIncludeTests:       false,
-			RequirePkgDocIncludeTests:      false,
-			RequireDocIncludeTests:         false,
-			RequireDocIgnoreExported:       false,
-			RequireDocIgnoreUnexported:     true,
-			StartWithNameIncludeTests:      false,
-			StartWithNameIncludeUnexported: false,
-			NoUnusedLinkIncludeTests:       false,
-		},
-	},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			require := require.New(t)
-
-			target := &model.RuleOptions{}
-			for _, source := range tt.sources {
-				config.TransferOptions(target, source)
-			}
-
-			require.Equal(tt.expected, target)
-		})
-	}
-}
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
@@ -107,6 +31,9 @@ func TestValidate(t *testing.T) {
 				Disable: []string{"foo", "bar", "baz"},
 				Include: []string{"(", ")"},
 				Exclude: []string{"(", ")"},
+				Options: &config.PlainRuleOptions{
+					MaxLenIgnorePatterns: []string{"(", "^foo$", ")"},
+				},
 			},
 			wantErr: []string{
 				`invalid default set "foo"; must be one of ["all" "basic" "none"]`,
@@ -114,6 +41,7 @@ func TestValidate(t *testing.T) {
 				`invalid rule name(s) to disable: ["foo" "bar" "baz"]`,
 				`invalid inclusion pattern(s): ["(" ")"]`,
 				`invalid exclusion pattern(s): ["(" ")"]`,
+				`invalid max-len ignore pattern(s): ["(" ")"]`,
 			},
 		},
 	}
