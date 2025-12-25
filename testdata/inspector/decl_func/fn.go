3d99865d95c39712bb257347c31e19a71ddb4c0e
Babak K. Shandiz <babakks@github.com>
2025-12-25 14:38:06 +0000

    test: assert extraction of method receiver base type name
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- testdata/inspector/decl_func/fn.go
+++ testdata/inspector/decl_func/fn.go
@@ -6,4 +6,31 @@ func Foo() {}
 type Bar string
 
 // godoc
-func (*Bar) BarFoo() {}
+func (Bar) BarFoo() {}
+
+// godoc
+func (*Bar) StarBarFoo() {}
+
+type Baz[T any] struct{}
+
+// godoc
+func (Baz[T]) BazFoo() {}
+
+// godoc
+func (*Baz[T]) StarBazFoo() {}
+
+type Yolo[T any, X any] struct{}
+
+// godoc
+func (Yolo[T, X]) YoloFoo() {}
+
+// godoc
+func (*Yolo[T, X]) StarYoloFoo() {}
+
+type Zoo = Bar
+
+// godoc
+func (Zoo) ZooFoo() {}
+
+// godoc
+func (*Zoo) StarZooFoo() {}
