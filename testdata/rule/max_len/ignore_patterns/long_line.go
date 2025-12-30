80a5d90126367fca2eb5d10a219a14262a235330
Babak K. Shandiz <babakks@github.com>
2025-12-30 00:08:34 +0000

    fix: improve issue position for `max-len`
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- testdata/rule/max_len/ignore_patterns/long_line.go
+++ testdata/rule/max_len/ignore_patterns/long_line.go
@@ -1,14 +1,14 @@
-// EXEMPT: this is a very very very very very very long line that would normally violate max-len //foo:bar // want `godoc line is too long \(67 > 40\)`
+// EXEMPT: this is a very very very very very very long line that would normally violate max-len
 // another very very very long line for a variable that is [ALSO EXEMPT] from checks
-// but only this line should be marked as long since it's not ignored.
+// but only this line should be marked as long since it's not ignored.  // want `godoc line is too long \(114 > 40\)`
 package max_len
 
-// EXEMPT: long package-level constant godoc that should be ignored by max-len //foo:bar // want `godoc line is too long \(67 > 40\)`
+// EXEMPT: long package-level constant godoc that should be ignored by max-len
 // another very very very long line for a variable that is [ALSO EXEMPT] from checks
-// but only this line should be marked as long since it's not ignored.
+// but only this line should be marked as long since it's not ignored.  // want `godoc line is too long \(114 > 40\)`
 const NoExemptConst = 0
 
-// EXEMPT: long variable godoc that should be ignored by max-len //foo:bar // want `godoc line is too long \(67 > 40\)`
+// EXEMPT: long variable godoc that should be ignored by max-len
 // another very very very long line for a variable that is [ALSO EXEMPT] from checks
-// but only this line should be marked as long since it's not ignored.
+// but only this line should be marked as long since it's not ignored.  // want `godoc line is too long \(114 > 40\)`
 var NoExemptVar = 0
